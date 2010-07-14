3abe7a71bbc00659790990bedeec339a7b01f8fa
Adrian Cuadros <adrian@innku.com>
2010-07-13 23:54:37 -0500

    Adds missing logo

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ require 'rho/rhoapplication'
 class AppApplication < Rho::RhoApplication
   
   
-  TABS = [  { :label => "Buscar", :action => '/app/Freight/search',:icon => "/public/images/search.png"},
+  TABS = [  { :label => "Buscar", :action => '/app/Freight/search',:icon => "/public/images/search.png", :reload => true},
             { :label => "Mis cargas",:action => '/app/MyFreight', :icon => "/public/images/truck.png"},
             { :label => "Salir",:action => '/app/Settings/logout', :icon => "/public/images/logout.png"}]
    
